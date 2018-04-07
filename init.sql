cc0c2dcb6926f0a54a5b93b8d6aa24f159945b10
Michael Prather <mprather1@gmail.com>
2018-04-07 00:31:05 +0000

    init.sql update && version.yml

--- init.sql
+++ init.sql
@@ -1,12 +1,9 @@
 CREATE TABLE models (
   ID SERIAL PRIMARY KEY,
-  name VARCHAR,
-  attribute INTEGER,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  optional VARCHAR,
+  email VARCHAR,
+  message VARCHAR,
   created_at TIMESTAMP without time zone default (now() at time zone 'utc')
-);
-
-CREATE TABLE users (
-  ID SERIAL PRIMARY KEY,
-    username VARCHAR,
-    password VARCHAR
 );
